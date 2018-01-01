7f4c4ecc44984ef2ee44dc11234ccdf3ff7b98b6
Stefan Loeffler <s.loeffler@custom-co.de>
2018-01-01 13:38:03 +0100

    deleted old code

--- src/ExpressiveTests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CustomCode.ExpressiveTests")]
-[assembly: AssemblyDescription("A collection of best practices and extensions for writing bdd style tests for c#.")]
-[assembly: Guid("5743b019-4b81-448e-869d-17c5af29410d")]
-[assembly: InternalsVisibleTo("CustomCode.Test.ExpressiveTests")]
--- src/Test.ExpressiveTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CustomCode.Test.ExpressiveTests")]
-[assembly: AssemblyDescription("Automated tests for the 'CustomCode.ExpressiveTests' assembly")]
-[assembly: Guid("24944d23-16ab-4919-9bde-d918727ad5c2")]
